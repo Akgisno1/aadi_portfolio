5454ed2977b5653ef7cf77b8bd60f7d5af5da3c8
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2024-10-22 15:12:31 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Anshul Kumar Godiwar",
+  title: "Full Stack Developer",
+  email: "anshulkumargodiwar2001@gmail.com",
+  gitHub: "Akgisno1",
+  instagram: "the.guy_who.asked",
+  linkedIn: "anshul-kumar-godiwar-885b0b268",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "computer desk with mouse";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "QueueOverFlow",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "FULL-STACK project using Next.js Server Actions which features secure Clerk authentication, a user reputation system and a unique global search.",
+    url: "https://queue-over-flow.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "RENTIFY",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      " MERN-STACK real-estate website also has Socket.io for real-time chatting with users, get chat notifications, user authentication with JWT tokens and Leaflet maps for displaying properties",
+    url: "https://rentify-8r7z.onrender.com/",
   },
   {
-    title: "My Resume Site",
+    title: "My PortFolio Website",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A Visually appealing and responsive portfolio website built using Next.js, Tailwind CSS leveraging GSAP animations.",
+    url: "https://akgport.vercel.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "StrayToStay",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A full-stack web application that connects stray animals with potential adopters, enabling users to find and adopt pets in their area.",
+    url: "https://straytostay.vercel.app/",
   },
 ];
 
